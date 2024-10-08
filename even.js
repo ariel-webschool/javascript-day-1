1bc511e61303e3cce80ae0243fe8ad64179db4fd
ariel-webschool <ariel@webschool.co.il>
2024-10-08 20:59:31 +0300

    Delete even.js

--- even.js
+++
@@ -1,8 +0,0 @@
-// Verifier si un numero est pair ou impair
-var number = prompt("Veuillez entrer votre nombre: ");
-
-if (number % 2 == 0) {
-  console.log("Le number: " + number + ", est paire.");
-} else {
-  console.log("Le number: " + number + ", est impaire.");
-}
