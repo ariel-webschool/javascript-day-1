100f86ab8f2da0ee93cc2ecf4705bf463c5e0443
ariel-webschool <ariel@webschool.co.il>
2024-10-08 19:06:42 +0300

    Add files via upload

---
+++ even.js
@@ -0,0 +1,8 @@
+// Verifier si un numero est pair ou impair
+var number = prompt("Veuillez entrer votre nombre: ");
+
+if (number % 2 == 0) {
+  console.log("Le number: " + number + ", est paire.");
+} else {
+  console.log("Le number: " + number + ", est impaire.");
+}
